e4ce8c66c75f25e4c8c9853c628590285d68bf01
moses1418 <mosesrodriguez1414@yahoo.com>
2021-06-12 17:41:09 -0500

    adding weather

---
+++ js/mapbox-utils.js
@@ -0,0 +1,72 @@
+mapboxgl.accessToken = MAPBOX_TOKEN;
+
+var map = new mapboxgl.Map({
+    container: "map",
+    style: 'mapbox://styles/mapbox/dark-v10',
+    center: [ -98.4936, 29.4241],
+    zoom: 12
+});
+
+let marker;
+mapEvent();
+let geocoder = setGeocoder();
+
+addGeacoderToMap(geocoder);
+
+function setGeocoder(){
+    return new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        marker: false
+    })
+}
+
+function addGeacoderToMap(geocoder){
+    map.addControl(geocoder);
+
+    geocoder.on("result", function (event) {
+        console.log(event.result.place_name);
+        // console.log(event);
+        // console.log(event.result.geometry.coordinates);
+
+        setMarker(event.result.geometry.coordinates);
+        marker.setPopup(displayPopup(event.result.place_name));
+    });
+
+}
+
+
+function setMarker(point) {
+
+    if (!marker){
+        marker = new mapboxgl.Marker().setLngLat(point).addTo(map);
+    } else {
+        marker.setLngLat(point);
+    }
+
+}
+
+
+function mapEvent() {
+    map.on("click", function (event) {
+        setMarker(event.lngLat);
+        console.log(event.lngLat);
+    });
+}
+
+
+function displayPopup(textDetails) {
+    return new mapboxgl.Popup().setHTML(`<p>${textDetails}</p>`).addTo(map);
+}
+
+
+
+
+
+
+
+
+
+
+
+
